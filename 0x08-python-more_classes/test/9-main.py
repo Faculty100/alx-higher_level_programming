2929dcfb8899607209f8a918e06714105dc02afb
Faculty100 <oprince503@gmail.com>
2023-04-05 07:44:54 -0700

    Update

---
+++ 0x08-python-more_classes/test/9-main.py
@@ -0,0 +1,9 @@
+able File  6 lines (5 sloc)  199 Bytes
+ 
+
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+my_square = Rectangle.square(5)
+print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
+print(my_square)
